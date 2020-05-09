f8937a5f6aa47cc55e097cd9abffc6ba4291e2db
d0ugr <doug@escm.ml>
2020-05-09 18:27:32 -0400

    Improve pan/zoom

--- client/src/App.js
+++ client/src/App.js
@@ -1,91 +1,89 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 import "./App.scss";
 
 function App() {
 
-  // const { viewState, setViewState } = useState({ zoom: 1 });
-  const [ mouseState, setMouseState ] = useState({ mouseDown: false, x: null, y: null });
-  const [ viewbox, setViewbox ] = useState({ x: 0, y: 0, w: 100, h: 100, scale: 1 });
+  const [ viewBoxState, setViewboxState ] = useState({
+    x:     0,
+    y:     0,
+    w:     100,
+    h:     100,
+    scale: 1
+  });
+  const [ clickState, setClickState ] = useState({
+    mouseDown: false,
+    x:         null,
+    y:         null,
+    vbx:       null,
+    vby:       null
+  });
 
   function onMouseDown(event) {
-    // Set the position of the mouse when clicking
+    // Save the position of the mouse when clicking
     //    to calculate relative panning distance
     //    as the mouse moves:
-    setMouseState({
+    setClickState({
       mouseDown: true,
-      x: event.clientX,
-      y: event.clientY
-    });
-    const svg = document.getElementById("svg");
-    setViewbox({
-      ...viewbox,
-      x: svg.viewBox.baseVal.x,
-      y: svg.viewBox.baseVal.y
+      x:         event.clientX,
+      y:         event.clientY,
+      vbx:       viewBoxState.x,
+      vby:       viewBoxState.y
     });
   }
 
   function onMouseUp(_event) {
-    setMouseState({
+    setClickState({
       mouseDown: false,
-      x: null,
-      y: null
+      x:         null,
+      y:         null,
+      vbx:       null,
+      yvb:       null
     });
   }
 
   function onMouseMove(event) {
-    if (mouseState.mouseDown) {
-      // Should be this:
-      // setViewbox({
-      //   ...viewbox,
-      //   x: viewbox.x + (event.clientX - mouseState.x) / 10,
-      //   y: viewbox.y + (event.clientY - mouseState.y) / 10
-      // });
-      // But doing this for now:
-      //    Because setting viewBox on the svg element in JSX is being stupid.
-      const svg = document.getElementById("svg");
-      svg.viewBox.baseVal.x = viewbox.x + (event.clientX - mouseState.x) / 10;
-      svg.viewBox.baseVal.y = viewbox.y + (event.clientY - mouseState.y) / 10;
+    if (clickState.mouseDown) {
+      setViewboxState({
+        ...viewBoxState,
+        x: clickState.vbx - (event.clientX - clickState.x) / 10,
+        y: clickState.vby - (event.clientY - clickState.y) / 10
+      });
     }
   }
 
   function onWheel(event) {
     // Scaling factor for the viewBox width/height:
     let scale = 1;
-    // The scroll wheel delta is +/-3 so it's also scaled
-    //    to make zooming not as drastic;
+    // Scale the scroll wheel delta to something not so drastic:
+    //    It's typically +/-3 which makes for large zoom steps.
     const wheelDelta = Math.abs(event.deltaY) / 2.75;
     // Scrolling up (zooming in):
     if (event.deltaY < 0) {
-      // setViewbox({
-      //   ...viewbox,
-      //   scale: viewbox.scale * event.deltaY
-      // });
-      scale *= wheelDelta;
+      scale = viewBoxState.scale / wheelDelta;
     // Scrolling down (zooming out):
     } else {
-      // setViewbox({
-      //   ...viewbox,
-      //   scale: viewbox.scale / event.deltaY
-      // });
-      scale /= wheelDelta;
+      scale = viewBoxState.scale * wheelDelta;
     }
-    // Scale the width/height of the viewBox:
-    //    This currently doesn't account for the top-left
-    //    position needing to be updated, so the objects don't remain centered.
-    const svg = document.getElementById("svg");
-    svg.viewBox.baseVal.width  *= scale;
-    svg.viewBox.baseVal.height *= scale;
+    setViewboxState({
+      ...viewBoxState,
+      scale
+    });
   }
+
+  // Prepare viewBox attribute values:
+  const viewBox = {};
+  viewBox.w = viewBoxState.w * viewBoxState.scale;
+  viewBox.h = viewBoxState.h * viewBoxState.scale;
+  viewBox.x = viewBoxState.x - viewBox.w / 2;
+  viewBox.y = viewBoxState.y - viewBox.h / 2;
 
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <svg
-        id="svg"
-        viewBox="0, 0, 100, 100"
+        viewBox={`${viewBox.x} ${viewBox.y} ${viewBox.w} ${viewBox.h}`}
         width={"100%"}
         height={"100%"}
         onMouseDown={onMouseDown}
@@ -93,7 +91,7 @@ function App() {
         onMouseMove={onMouseMove}
         onWheel={onWheel}
       >
-        <ellipse cx={50} cy={50} rx={30} ry={20} fill="purple"></ellipse>
+        <ellipse cx={0} cy={0} rx={30} ry={20}></ellipse>
       </svg>
     </div>
   );

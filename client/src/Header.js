d8bb7d1a66ca1c9d7bb3b97af12461457305d8a2
d0ugr <doug@escm.ml>
2020-05-12 22:03:47 -0400

    Separate Header styles

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import "./Header.scss";
+
 
 
 function Header(props) {
 
   return (
-    <header className="App-header">
+    <header className="app-header">
       <strong>WB2020</strong>&nbsp;&bull;&nbsp;{props.sessionName}
     </header>
   );

964dacef1e68811b90cd5be310df3a1134d00c18
d0ugr <doug@escm.ml>
2020-05-13 00:34:43 -0400

    Add default session

--- server/db/seeds/seeds.sql
+++ server/db/seeds/seeds.sql
@@ -3,12 +3,13 @@
 -- password: $2b$10$zUr5GSoayQGyHxrhRU512ekcmmdQe0dcI7v3BJlbukUz7U6IOVQim
 -- 123:      $2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG
 
-INSERT INTO sessions (username, password, email, name) VALUES
-  ('123', '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', 't@e.st', 'Jazz Session'),
-  ('123', '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', 't@e.st', 'Mad Sesh'),
-  ('421', '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', '4@2.0', 'Max and Relax'),
-  ('421', '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', '4@2.0', 'Chillin Yo'),
-  ('421', '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', '4@2.0', 'Let''s go to Sleep')
+INSERT INTO sessions (session_key, username, password, email, name) VALUES
+  ('default',    'default', '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', 'de@fau.lt', 'Default Session'),
+  ('jazz',       '123',     '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', 't@e.st', 'Jazz Session'),
+  ('mad_sesh',   '123',     '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', 't@e.st', 'Mad Sesh'),
+  ('max+relax',  '421',     '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', '4@2.0', 'Max and Relax'),
+  ('chillin',    '421',     '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', '4@2.0', 'Chillin Yo'),
+  ('sleeeeeeps', '421',     '$2b$10$WMtxQHMdAIcArSAe8B.dPu3FJ56M2.MenoSGja8Xx/6PGkDcFgrzG', '4@2.0', 'Let''s go to Sleep')
 ;
 
 INSERT INTO cards (session_id, content) VALUES
@@ -26,10 +27,10 @@ INSERT INTO participants (session_id, sequence, name) VALUES
   (1, 1, 'Linh Nguyen'),
   (1, 2, 'Doug Ross'),
   (1, 3, 'X Æ A-12'),
-  (1, 1, 'Herbert'),
-  (1, 2, 'Bob Knickeryanker'),
-  (1, 3, 'Let''s Play Checkers'),
-  (1, 4, 'Magical Bongo'),
-  (1, 5, 'Password'),
-  (1, 6, 'Chowder Crowder')
+  (2, 1, 'Herbert'),
+  (2, 2, 'Bob Knickeryanker'),
+  (2, 3, 'Let''s Play Checkers'),
+  (2, 4, 'Magical Bongo'),
+  (2, 5, 'Password'),
+  (2, 6, 'Chowder Crowder')
 ;

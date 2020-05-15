befbda4a62c05e37b8913ab75345131d827c4e11
d0ugr <doug@escm.ml>
2020-05-15 13:29:46 -0400

    Update create database scripts

---
+++ server/db/create_development_database.sql
@@ -0,0 +1,3 @@
+CREATE USER wb2020_development WITH NOSUPERUSER PASSWORD 'wb2020_development';
+CREATE DATABASE wb2020_development OWNER wb2020_development;
+GRANT ALL ON DATABASE wb2020_development TO wb2020_development;
